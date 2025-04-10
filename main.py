ffc08e919516c9f026b2cdf04a72708b49c07a50
Arya Wiratama <aryawiratama2401@gmail.com>
2025-04-11 00:01:58 +0700

    fastdc

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastdc import FastBot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+BOT_KEY = os.getenv('API_BOT_KEY')
+
+bot = FastBot(token=BOT_KEY)
+bot.auto_reply(trigger="Hello Semuanya", response='Halo Juga')
+bot.train_bot()
+bot.ai_chat(api_key_usr=API_KEY)
+bot.run()
