10ff601b4b683bc4d89dbe099d3d0c563d554039
Arya Wiratama <aryawiratama2401@gmail.com>
2025-04-11 14:12:45 +0700

    add doc string on python files

--- fastdc/trainer.py
+++ fastdc/trainer.py
@@ -16,6 +16,15 @@ class FastdcTrainer:
         self.data_path = data_path
 
     def train(self):
+
+        """
+            Trains the chatbot using a conversation dataset stored in a text file.
+
+            This method reads training data from `self.data_path`, splits it into lines, 
+            and feeds it to a `ChatterBot` ListTrainer instance to learn responses.
+
+        """
+
         trainer = ListTrainer(self.chatbot)
         if not os.path.exists(self.data_path):
             raise FileNotFoundError(f"Training data file '{self.data_path}' not found.")
@@ -24,4 +33,12 @@ class FastdcTrainer:
         trainer.train(conversation)
 
     def get_response(self, message):
+        """ 
+            Generates a response from the trained chatbot based on the user's input message. 
+            Parameters:
+            ----------
+            message : str
+                The input message or question to send to the chatbot.
+
+        """
         return str(self.chatbot.get_response(message))
