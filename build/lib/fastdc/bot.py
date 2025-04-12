99875a574f004ed44e7858b88da3c91ecee1eaa7
Arya Wiratama <aryawiratama2401@gmail.com>
2025-04-12 15:19:15 +0700

    update fastdc 1.1 to pypi

--- build/lib/fastdc/bot.py
+++ build/lib/fastdc/bot.py
@@ -10,6 +10,22 @@ class FastBot:
         self.trainer.train()
 
     def auto_reply(self, trigger, response):
+        
+        """
+        
+            Sets up an auto-reply feature for the bot. When a message containing the 
+            specified trigger word or phrase is detected, the bot responds with the 
+            provided response message.
+
+            Parameters:
+            ----------
+            trigger : str
+                The word or phrase that will trigger the auto-reply.
+            
+            response : str
+                The message that the bot will send in response when the trigger is detected.
+        """
+        
         @self.bot.event
         async def on_message(message):
             if message.author.bot:
@@ -18,7 +34,58 @@ class FastBot:
                 await message.channel.send(response)
             await self.bot.process_commands(message)
             
+    def welcome_member(self):
+        
+        """
+            Sends a welcome message to new members who join the Discord server.
+            The message is sent in the server's system channel if it exists.
+        """
+        
+        @self.bot.event
+        async def on_member_join(member):
+            channel = member.guild.system_channel
+            
+            if not channel:
+                for ch in member.guild.text_channels:
+                    if ch.permissions_for(member.guild.me).send_messages:
+                        channel = ch
+                        break
+                    
+            if channel:
+                await channel.send(f"Hello {member.name}, Welcome to Server! 👋")
+                
+    def leave_member(self):
+        
+        """
+            Sends a farewell message to the system channel (or first accessible channel) 
+            when a member leaves the server.
+        """
+        
+        @self.bot.event
+        async def on_member_remove(member):
+            channel = member.guild.system_channel
+            
+            if not channel:
+                for ch in member.guild.text_channels:
+                    if ch.permissions_for(member.guild.me).send_messages:
+                        channel = ch
+                        break
+            
+            if channel:
+                await channel.send(f"{member.name} has left the server 🖐️")
+            
     def ai_chat(self, api_key_usr):
+        
+        """
+            This command allows users to interact with an LLM (LLaMA 3.3 70B) directly from Discord using the !ai command.
+
+            Parameters:
+            ----------
+            api_key_usr : str
+                Your Groq API key used to authenticate and access the AI model.
+            
+        """
+        
         from groq import Groq
 
         @self.bot.command()
@@ -51,11 +118,19 @@ class FastBot:
             await ctx.send(chat_completion.choices[0].message.content)
             
     def train_bot(self):
+        
+        """
+            Sets up a command to interact with a pre-trained conversational model 
+            using the ChatterBot library.
+        """
         @self.bot.command()
         async def askbot(ctx, *, message):
             response = self.trainer.get_response(message)
             await ctx.send(response)
 
     def run(self):
+        """
+            Starts the Discord bot using the provided token.
+        """
         self.bot.run(self.token)
                     
--- build/lib/fastdc/trainer.py
+++ build/lib/fastdc/trainer.py
@@ -7,7 +7,7 @@ try:
     spacy.load("en_core_web_sm")
 except OSError:
     from spacy.cli import download
-    print("🔧 Downloading 'en_core_web_sm' model...")
+    print("Downloading 'en_core_web_sm' model...")
     download("en_core_web_sm")
 
 class FastdcTrainer:
@@ -16,6 +16,15 @@ class FastdcTrainer:
         self.data_path = data_path
 
     def train(self):
+
+        """
+            Trains the chatbot using a conversation dataset stored in a text file.
+
+            This method reads training data from `self.data_path`, splits it into lines, 
+            and feeds it to a `ChatterBot` ListTrainer instance to learn responses.
+
+        """
+
         trainer = ListTrainer(self.chatbot)
         if not os.path.exists(self.data_path):
             raise FileNotFoundError(f"Training data file '{self.data_path}' not found.")
@@ -24,4 +33,12 @@ class FastdcTrainer:
         trainer.train(conversation)
 
     def get_response(self, message):
+        """ 
+            Generates a response from the trained chatbot based on the user's input message. 
+            Parameters:
+            ----------
+            message : str
+                The input message or question to send to the chatbot.
+
+        """
         return str(self.chatbot.get_response(message))
